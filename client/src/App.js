a2ac30e313c048303a1de84d2f314d6327289ab3
yonkodhia <yasyasdhia@gmail.com>
2023-10-26 18:34:12 +0100

    off

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Signin from './components/Signin/Signin'
 
 const App = () => {
   return (
     <div>
-      hello
+        
+      <Routes>
+        <Route path="/SIGNin" element={<Signin />}></Route>
+        </Routes>
+    
     </div>
-  )
+  
+     
+      
+   
+  );
 }
 
 export default App
