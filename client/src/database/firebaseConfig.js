a64786c72d411ff6b9c462d2b169359f864e8a36
leith ayadi <leithayadi188@gmail.com>
2023-10-26 09:20:04 -1100

    done

---
+++ client/src/database/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHCJduRp8gSWc5pYhOCCd6_xlWZkZX-dg",
+  authDomain: "l-avocato-7fd6f.firebaseapp.com",
+  databaseURL: "https://l-avocato-7fd6f-default-rtdb.firebaseio.com",
+  projectId: "l-avocato-7fd6f",
+  storageBucket: "l-avocato-7fd6f.appspot.com",
+  messagingSenderId: "609527487048",
+  appId: "1:609527487048:web:b6bba36309077adbfad67d",
+  measurementId: "G-VBYHLDEW3Y"
+};
+
+
+export const app = initializeApp(firebaseConfig);
