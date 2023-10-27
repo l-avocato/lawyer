76f2a233bcea90f8fe05c6b2679d63be2fd8a909
leith ayadi <leithayadi188@gmail.com>
2023-10-27 04:22:28 -1100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from "./components/LandingPage/Landing";
 
+import AllClient from './components/AllClient/AllClient.jsx';
+
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/landing" element={<Landing />} />
+
+
+
+
+
+         <Route path="/allClient" element={<AllClient/>} />
+
+
+          
+
+
         </Routes>
       </Router>
     </div>
