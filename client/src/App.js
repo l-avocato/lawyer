e692245d4fb1ef3809a9d877a9451830c2d1d184
yonkodhia <yasyasdhia@gmail.com>
2023-10-27 14:30:53 +0100

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from "./components/LandingPage/Landing";
+import Signin from './components/Signin/Signin';
+import Signup from './components/Signin/Signup';
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/landing" element={<Landing />} />
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/Signup" element={<Signup />} />
         </Routes>
       </Router>
     </div>
