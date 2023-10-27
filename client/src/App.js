70abd212386a6f806ddc3fbd40f8a0f8c292d90f
azizarf <aziz95arfaoui@gmail.com>
2023-10-27 16:23:30 +0100

    client profile in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./components/LandingPage/Landing";
+import ClientDetails from "./components/clientDetails/ClientDetails";
 
 const App = () => {
   return (
@@ -8,10 +9,13 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/landing" element={<Landing />} />
+
+          <Route path="/clientDetails" element={<ClientDetails />} />
+          
         </Routes>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
