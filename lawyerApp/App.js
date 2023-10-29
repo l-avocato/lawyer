4cdaeabdde8838c19bd4095436d7fb81de7725e7
leith ayadi <leithayadi188@gmail.com>
2023-10-29 00:23:01 -1100

    update

--- lawyerApp/App.js
+++ lawyerApp/App.js
@@ -1,19 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ProfilDetails from './components/ProfilDetails';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ProfilDetails" component={ProfilDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+ 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
