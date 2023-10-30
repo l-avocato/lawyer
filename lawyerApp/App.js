ecbce299d91291e7ef85be7f808ad46c85fa2003
leith ayadi <leithayadi188@gmail.com>
2023-10-30 06:41:06 -1100

    update

--- lawyerApp/App.js
+++ lawyerApp/App.js
@@ -1,57 +1,40 @@
 // import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {ProfilDetails} from './screens';
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import ProfilDetails from "./screens/ProfilDetails";
+import { StatusBar } from "expo-status-bar";
 
 //screens
-import ProfilDetails from './components/ProfilDetails';
-import Login from './screens/Login';
-import Signup from './screens/SignUp';
-import Welcome from './screens/Welcome';
+
+import Login from "./screens/Login";
+import Signup from "./screens/SignUp";
+import Welcome from "./screens/Welcome";
 
 const Stack = createNativeStackNavigator();
 
-
 export default function App() {
   return (
-     
-
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='ProfilDetails'> 
-      
-      
+      <Stack.Navigator initialRouteName="ProfilDetails">
+        <Stack.Screen
+          name="ProfilDetails"
+          component={ProfilDetails}
+          options={{
+            headerShown: false,
+          }}
+        />
 
-        <Stack.Screen name="ProfilDetails" component={ProfilDetails} options={{
-            headerShown: false
-          }} />
-        
-          
-        <Welcome />
+        <Stack.Screen
+          name="welcome"
+          component={Welcome}
+          options={{
+            headerShown: false,
+          }}
+        />
 
-
-
+       
       </Stack.Navigator>
     </NavigationContainer>
-
- 
-  )
+  );
 }
-
-
-    
- 
-
-
-
-
-
-
-
-
-
-
-
