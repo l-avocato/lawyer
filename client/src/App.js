d9e64b463d958988a92cbea18d76c2633a86179a
Mouhibdaly <mouhibfeydi195@gmail.com>
2023-10-30 08:51:16 +0100

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Landing from "./components/LandingPage/Landing";
-import Signin from './components/Signin/Signin';
-import Signup from './components/Signin/Signup';
+import Signin from "./components/Signin/Signin";
+import Signup from "./components/Signin/Signup";
 import ClientDetails from "./components/clientDetails/ClientDetails";
 import NavbarDashboard from "./components/NavbarDashboard/NavbarDashboard";
 import SidebarDash from "./components/SidebarDash/SidebarDash";
 import Footer from "./components/Footer/Footer";
 import AllClient from "./components/AllClient/AllClient.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Landing from "./components/LandingPage/Landing.jsx";
+import Landing from "./components/LandingPage/landing";
+
 
 const App = () => {
   return (
@@ -20,17 +20,12 @@ const App = () => {
           <Route path="/nav" element={<Navbar />} />
           <Route path="/Signin" element={<Signin />} />
           <Route path="/Signup" element={<Signup />} />
-
           <Route path="/clientDetails" element={<ClientDetails />} />
           <Route path="/allClient" element={<AllClient />} />
           <Route path="/sidebar" element={<SidebarDash />} />
         </Routes>
       </Router>
     </div>
-  
-     
-      
-   
   );
 };
 
