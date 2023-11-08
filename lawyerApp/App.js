c135b62789be95a4a79e53dd23698dac7d497cf5
azizarf <aziz95arfaoui@gmail.com>
2023-11-08 11:06:40 +0100

    done

--- lawyerApp/App.js
+++ lawyerApp/App.js
@@ -29,7 +29,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="SearchListings"
+        initialRouteName="SearchListings" 
         component={SearchListings}
         options={{ headerShown: false }}>
         <Stack.Screen
--- lawyerApp/screens/SearchListings.jsx
+++ lawyerApp/screens/SearchListings.jsx
@@ -1,12 +1,80 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity ,ScrollView} from "react-native";
+import React, { useState,useEffect} from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+  FlatList,
+} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import {FIREBASE_DB } from '../firebaseConfig'
+import { collection, getDocs} from "firebase/firestore";
+
 
 const SearchListings = () => {
   const handleArrowClick = () => {
     // Handle arrow icon click here
   };
+  const handleButtonPress = () => {
+    // Handle button press here
+    console.log("Button pressed");
+    // You can add your custom logic here
+  };
+  const [isHeartPressed, setIsHeartPressed] = useState(false);
+  const lawyersCollectionRef = collection(FIREBASE_DB, "lawyers");
+  const [lawyers, setLawyers]= useState([]);
+  const getLawyers = async () => {
+    try {
+      const result = await getDocs(lawyersCollectionRef);
+      const lawyers = result.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
 
+      })
+      );
+      setLawyers(lawyers);
+      console.log("this is lawyers",lawyers);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(()=>{
+    getLawyers()
+
+  },[])
+  const renderItem = ({ item }) => (
+    <View style={styles.lawyerView1}>
+      <Image source={{uri : item.imageUrl}} style={styles.lawyerImage} />
+      <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
+        <Text style={styles.buttonText}>{item.fullName}</Text>
+      </TouchableOpacity>
+      <Text style={styles.additionalText}>{item.category}</Text>
+      <View style={styles.starIcon}>
+        <Icon name="star" size={18} color="#D5B278" />
+        <Text style={styles.textt}>{4.5}</Text>
+      </View>
+      <View style={styles.locationIcon}>
+        <Icon name="map-marker" size={20} color="blue" />
+        <Text style={styles.textt}>1.5km</Text>
+      </View>
+      <View style={styles.priceIcon}>
+        <Icon name="dollar" size={18} color="green" />
+        <Text style={styles.textt}>{item.Price}</Text>
+      </View>
+      <TouchableOpacity
+        onPress={() => setIsHeartPressed(!isHeartPressed)}
+        style={styles.heartIcon}
+      >
+        {isHeartPressed ? (
+          <Icon name="heart" size={20} color="red" />
+        ) : (
+          <Icon name="heart-o" size={20} color="red" />
+        )}
+      </TouchableOpacity>
+    </View>
+  );
   return (
     <View>
       <View style={styles.header}>
@@ -18,10 +86,13 @@ const SearchListings = () => {
 
       {/* Body View */}
       <ScrollView contentContainerStyle={styles.body1}>
-      <View style={styles.greyView}>
-        {/* View with grey background and 200 height */}
-      </View>
-    </ScrollView>
+      <FlatList
+      data={lawyers}
+      keyExtractor={(item) => item.id}
+      renderItem={renderItem}
+      contentContainerStyle={styles.body1}
+    />
+      </ScrollView>
     </View>
   );
 };
@@ -29,15 +100,15 @@ const SearchListings = () => {
 const styles = StyleSheet.create({
   header: {
     backgroundColor: "black",
-    height: 150, 
-    flexDirection: "column", 
-    justifyContent: "flex-end", 
-    alignItems: "center", 
+    height: 150,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "center",
     padding: 10,
   },
   arrowIcon: {
-    alignSelf: "flex-start", 
-    transform: [{ rotate: "360deg" }], 
+    alignSelf: "flex-start",
+    transform: [{ rotate: "360deg" }],
   },
   headerText: {
     color: "white",
@@ -46,11 +117,80 @@ const styles = StyleSheet.create({
   body1: {
     flexGrow: 1,
   },
-  greyView: {
-    backgroundColor: 'grey',
-    height: 200,
+  lawyerView1: {
+    backgroundColor: "white",
+    height: 150,
+    padding: 10,
+    marginBottom: 10,
+    flexDirection: "row", // To align image and content side by side
+    alignItems: "center",
   },
-
+  lawyerView2: {
+    backgroundColor: "white",
+    height: 150,
+    padding: 10,
+    marginBottom: 10,
+    flexDirection: "row", // To align image and content side by side
+    alignItems: "center",
+  },
+  lawyerView3: {
+    backgroundColor: "white",
+    height: 150,
+    padding: 10,
+    marginBottom: 10,
+    flexDirection: "row", // To align image and content side by side
+    alignItems: "center",
+  },
+  lawyerImage: {
+    width: 100,
+    height: 100,
+    marginRight: 10,
+    borderRadius: 20,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+    marginBottom: 60,
+    marginLeft: 0,
+  },
+  buttonText: {
+    color: "#D5B278",
+    textAlign: "center",
+    fontSize: 20,
+  },
+  additionalText: {
+    position: "absolute",
+    color: "grey",
+    fontSize: 16,
+    marginBottom: 1,
+    right: 200,
+  },
+  starIcon: {
+    position: "absolute",
+    top: 85, // Adjust the top position as needed
+    right: 275, // Adjust the right position as needed
+  },
+  locationIcon: {
+    position: "absolute",
+    top: 85, // Adjust the top position as needed
+    right: 205, // Adjust the right position as needed
+  },
+  priceIcon: {
+    position:"absolute",
+    top: 86,
+    right: 160,
+  },
+  heartIcon: {
+    position:"absolute",
+    top: 30, // Adjust the top position as needed
+    right: 20, // Adjust the right position as needed
+  },
+  textt: {
+    // position: "absolute",
+    top: -15, // Adjust the top position as needed
+    right: -20, // Adjust the right position as needed
+  },
 });
 
 export default SearchListings;
