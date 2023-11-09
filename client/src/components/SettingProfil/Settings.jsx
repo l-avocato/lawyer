80016df5c2fe813dcea2f01fad6d43e7585b8c07
Mouhibdaly <mouhibfeydi195@gmail.com>
2023-11-09 12:51:35 +0100

    done

--- client/src/components/SettingProfil/Settings.jsx
+++ client/src/components/SettingProfil/Settings.jsx
@@ -1,44 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import axios from "axios";
 import NavbarDashboard from "../NavbarDashboard/NavbarDashboard";
 import { db } from "../../firebaseconfig";
 import { updateDoc, doc } from "firebase/firestore";
 import SidebarDash from "../SidebarDash/SidebarDash";
+import defaultpic from "../../assets/images/default.png";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
-const Settings = () => {
+const ProfileDetails = () => {
   const [papers, setPapers] = useState("");
   const [fullName, setFullName] = useState("");
-  const [adress, setAdress] = useState("");
+  const [address, setAddress] = useState("");
+  const [mapVisible, setMapVisible] = useState(false);
+  const [map, setMap] = React.useState(null);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBJCGfd16u9ZMNd8v2rmZYqz6s2b4VGA1c",
+  });
 
   const handleFile = async (e) => {
     const formData = new FormData();
     formData.append("file", e.target.files[0]);
     formData.append("upload_preset", "oztadvnr");
-    await axios
-      .post("https://api.cloudinary.com/v1_1/dl4qexes8/upload", formData)
-      .then((response) => {
-        setPapers(response.data["secure_url"]);
-      })
-      .catch((error) => {
-        throw error;
-      });
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/dl4qexes8/upload",
+        formData
+      );
+      setPapers(response.data["secure_url"]);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
   };
+
 
   const lawyersCollectionRef = doc(db, "lawyers", "EIKiyaY44S1xWenxPxVh");
   const updateLawyerData = async () => {
     try {
       await updateDoc(lawyersCollectionRef, {
         fullName: fullName,
-        adress: adress,
+        address: address,
         imageUrl: papers,
       });
     } catch (error) {
-      console.log("Error updating lawyer", error);
+      console.error("Error updating lawyer", error);
     }
   };
+
+  const handleMapToggle = () => {
+    setMapVisible(!mapVisible);
+  };
+
   return (
     <div>
       <NavbarDashboard />
+      <SidebarDash />
 
       <div style={{ flex: 1 }}>
         <form
@@ -50,8 +67,7 @@ const Settings = () => {
             marginLeft: "35rem",
             gap: "0.5rem",
             marginBottom: "2rem",
-            border: "0.1rem solid #ccc",
-            borderRadius: "8px",
+            marginTop: "-6rem",
           }}
         >
           <p
@@ -62,18 +78,22 @@ const Settings = () => {
               alignItems: "center",
               justifyContent: "center",
               gap: "1rem",
-              marginLeft :"-60rem"
+              marginLeft: "-60rem",
+              marginTop: "6rem",
             }}
           >
             Edit Profile
           </p>
-
           <div className="flex">
             <img
-              src={papers}
+              src={papers || defaultpic}
               alt=""
               className="imageUrl"
-              style={{ width: "8rem", height: "8rem" }}
+              style={{
+                width: "8rem",
+                height: "8rem",
+                backgroundSize: "cover",
+              }}
             />
             <input
               type="file"
@@ -95,32 +115,48 @@ const Settings = () => {
                 setFullName(e.target.value);
               }}
             />
-            <label className="form-label" htmlFor="form6Example3">
-          
-            </label>
+            <label className="form-label" htmlFor="form6Example3"></label>
           </div>
           <div className="form-outline mb-2">
             <input
               type="text"
-              placeholder="Adress"
+              placeholder="Address"
               id="form6Example3"
               className="form-control"
               style={{ fontSize: "18px" }}
               onChange={(e) => {
-                setAdress(e.target.value);
+                setAddress(e.target.value);
               }}
             />
-            <label className="form-label" htmlFor="form6Example3">
-
-            </label>
+            <label className="form-label" htmlFor="form6Example3"></label>
           </div>
+          {mapVisible && (
+            <div
+              className="map"
+              style={{
+                width: "100%",
+                height: "400px",
+                backgroundColor: "lightgray",
+              }}
+            >
+              <GoogleMap
+                mapContainerClassName="map"
+                // center={center}
+                zoom={10}
+        
+              >
+                <Marker position={{ lat: 18.52043, lng: 73.856743 }} />
+              </GoogleMap>
+            </div>
+          )}
 
           <button
-            type="submit"
+            type="button"
             style={{
               backgroundColor: "gold",
               color: "black",
               fontSize: "1.3rem",
+              marginTop: "8rem",
             }}
             onClick={(e) => {
               e.preventDefault();
@@ -129,10 +165,22 @@ const Settings = () => {
           >
             Update
           </button>
+          <button
+            type="button"
+            style={{
+              backgroundColor: "green",
+              color: "white",
+              fontSize: "1.3rem",
+              marginTop: "2rem",
+            }}
+            onClick={handleMapToggle}
+          >
+            Toggle Map
+          </button>
         </form>
       </div>
     </div>
   );
 };
 
-export default Settings;
+export default ProfileDetails;
