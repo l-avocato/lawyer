70dc5e45168d460149f4e81c6c5e254bdcf9b4ab
leith ayadi <leithayadi188@gmail.com>
2023-11-15 08:58:48 -1100

    ok

--- client/src/components/StepsCases/InformationPhase.jsx
+++ client/src/components/StepsCases/InformationPhase.jsx
@@ -1,18 +1,25 @@
 import "./dashboardStyle.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SidebarDash from "../SidebarDash/SidebarDash";
 import NavbarDashboard from "../NavbarDashboard/NavbarDashboard";
 
 import axios from "axios";
 import FolderIcon from '@mui/icons-material/Folder';
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 
 
 const InformationPhase = () => {
 
-  const [files, setFiles] = useState("")
+  // const [files, setFiles] = useState("")
   const [path, setPath] = useState("document")
+  const [folders,setFolders]=useState([])
+  const [files, setFiles] = useState([])
+  const [newName, setNewName] = useState("")
+  const [nameFile, setNameFile] = useState("")
+  const [refrech, setRefrech] =useState(false)
 
   const getFile = async (e) => {
     
@@ -33,6 +40,58 @@ const InformationPhase = () => {
 
   }
 
+const fetchFolder = async()=> {
+  try {
+    const response = await axios.get("http://localhost:1128/api/folder/getAll/folder")
+    console.log(response.data);
+    setFolders(response.data)
+
+  } catch (error) {
+    console.log(error);
+  }
+} 
+
+const fetchFile = async()=> {
+  try {
+    const response = await axios.get("http://localhost:1128/api/file/getAll/files")
+    console.log(response.data);
+    setFiles(response.data)
+    set
+
+  } catch (error) {
+    console.log(error);
+  }
+} 
+
+useEffect(()=>{
+  fetchFolder();
+  fetchFile()
+},[refrech])
+
+
+  const addFolder = async ()=>{
+    try {
+       const response = await axios.post("http://localhost:1128/api/folder/add" , {
+        name:newName,
+       })
+       setRefrech(!refrech)
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
+  // const addFile = async ()=>{
+  //   try {
+  //      const response = await axios.post("http://localhost:1128/api/file/add" , {
+  //       name:nameFile,
+  //      })
+  //      setRefrech(!refrech)
+
+  //   } catch (error) {
+  //     console.log(error)
+      
+  //   }
+  // }
 
   return (
     <div
@@ -230,36 +289,23 @@ const InformationPhase = () => {
             <div className="block-folder">
               <div style={{display:'flex', justifyContent:'space-between', padding:'1rem' , overflowY:'scrol'}}>
                 <p style={{fontSize:'20px'}}> Folder Storage </p>
-                 <button style={{border:'none', width:'40px', borderRadius:'50%', fontSize:'30px', backgroundColor:'goldenrod'}}> + </button>
+                 <button style={{border:'none', width:'40px', borderRadius:'50%', fontSize:'30px', backgroundColor:'goldenrod'}} onClick={()=>{addFolder()}}> + </button>
               </div>
               <div style={{display:'flex', flexDirection:'column', gap:'1rem', paddingLeft:'2rem' }}>
-              <div style={{display:'flex', flexDirection:'row' , alignItems:'center', alignText:'center', height:'3rem' }}> 
-              <FolderIcon style={{color:"goldenrod", height:"100%", width:'20%'}}/>
-               <p style={{height:'50%' , margin:0}}>Main File 2  </p>
-              </div>
-              <div style={{height:'.01rem', width:'80%', backgroundColor:'grey' }}></div>
+                {folders.map((folder,i)=>{
+                  return (
+                    <>
+                    <div style={{display:'flex', flexDirection:'row' , alignItems:'center', alignText:'center', height:'3rem' }} key={i}> 
+                    <FolderIcon style={{color:"goldenrod", height:"100%", width:'20%'}}/>
+                     <p style={{height:'50%' , margin:0}}>{folder.name} </p>
+                    </div>
+                    <div style={{height:'.01rem', width:'80%', backgroundColor:'grey' }}></div>
+                    </>
+                  )
+                })}
+            
              
-              <div style={{display:'flex', flexDirection:'row' , alignItems:'center', alignText:'center', height:'3rem' }}> 
-              <FolderIcon style={{color:"goldenrod", height:"100%", width:'20%'}}/>
-               <p style={{height:'50%' , margin:0}}>Main File 3 </p>
-              </div>
-              <div style={{height:'.01rem', width:'80%', backgroundColor:'grey' }}></div>
              
-              <div style={{display:'flex', flexDirection:'row' , alignItems:'center', alignText:'center', height:'3rem' }}> 
-              <FolderIcon style={{color:"goldenrod", height:"100%", width:'20%'}}/>
-               <p style={{height:'50%' , margin:0}}>Main File   4 </p>
-              </div>
-              <div style={{height:'.01rem', width:'80%', backgroundColor:'grey' }}></div>
-              <div style={{display:'flex', flexDirection:'row' , alignItems:'center', alignText:'center', height:'3rem' }}> 
-              <FolderIcon style={{color:"goldenrod", height:"100%", width:'20%'}}/>
-               <p style={{height:'50%' , margin:0}}>Main File   5 </p>
-              </div>
-              <div style={{height:'.01rem', width:'80%', backgroundColor:'grey' }}></div>
-              <div style={{display:'flex', flexDirection:'row' , alignItems:'center', alignText:'center', height:'3rem' }}> 
-              <FolderIcon style={{color:"goldenrod", height:"100%", width:'20%'}}/>
-               <p style={{height:'50%' , margin:0}}>Main File   6 </p>
-              </div>
-              <div style={{height:'.01rem', width:'80%', backgroundColor:'grey' }}></div>
 
              
               
@@ -267,29 +313,94 @@ const InformationPhase = () => {
               </div>
             </div>
             <div className="block-note">
+              <div style={{display:'flex', justifyContent:'space-between', padding:'1rem'}}>
+                <p style={{fontSize:'20px', fontWeight:'bold'}}>Add Notes</p>
+                <button style={{border:'none', borderRadius:'50%', backgroundColor:'goldenrod', width:'40px', fontSize:'30px'}}> + </button>
+
+
+              </div>
+              <div style={{display:'flex', justifyContent:'center'}}>
+              <div style={{width:'47vh', height:'15vh', display:'flex', }}>
+              <div class="card">
+  <div class="comments">
+   
+    <div class="comment-container">
+      <div class="user">
+        <div class="user-pic">
+          <svg fill="none" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linejoin="round" fill="#707277" stroke-linecap="round" stroke-width="2" stroke="#707277" d="M6.57757 15.4816C5.1628 16.324 1.45336 18.0441 3.71266 20.1966C4.81631 21.248 6.04549 22 7.59087 22H16.4091C17.9545 22 19.1837 21.248 20.2873 20.1966C22.5466 18.0441 18.8372 16.324 17.4224 15.4816C14.1048 13.5061 9.89519 13.5061 6.57757 15.4816Z"></path>
+            <path stroke-width="2" fill="#707277" stroke="#707277" d="M16.5 6.5C16.5 8.98528 14.4853 11 12 11C9.51472 11 7.5 8.98528 7.5 6.5C7.5 4.01472 9.51472 2 12 2C14.4853 2 16.5 4.01472 16.5 6.5Z"></path>
+          </svg>
+        </div>
+        <div class="user-info">
+          <span>Yassine Zanina</span>
+        </div>
+      </div>
+      <p class="comment-content">
+        I've been using this product for a few days now and I'm really impressed! The interface is intuitive and easy to
+      </p>
+    </div>
+  </div>
+
+  
+</div>
+
+              </div>
+
                </div>
+               <div class="text-box">
+    <div class="box-container" >
+      <textarea placeholder="Add Notes"></textarea>
+      <div>
+        <div class="formatting" style={{display:'flex', justifyContent:'flex-end'}} >
+         
+         
+          
+          <button type="submit" class="send" title="Send">
+            <svg fill="none" viewBox="0 0 24 24" height="18" width="18" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#ffffff" d="M12 5L12 20"></path>
+              <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#ffffff" d="M7 9L11.2929 4.70711C11.6262 4.37377 11.7929 4.20711 12 4.20711C12.2071 4.20711 12.3738 4.37377 12.7071 4.70711L17 9"></path>
+            </svg>
+          </button>
+          <AttachFileIcon/>
+        </div>
+      </div>
+    </div>
+  </div>
+               </div>
+               
             </div>
+            
             <div style={{display:'flex', justifyContent:'flex-end'}}>
               <div className="block-file">
               <div style={{display:'flex',  padding:'1rem' , flexDirection:"column",overflowY:'scrol'}}>
                 <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
                 <p style={{fontSize:'20px', margin:0}}> File Storage </p>
-                 <button style={{border:'none', width:'40px', borderRadius:'50%', fontSize:'30px', backgroundColor:'goldenrod'}}> + </button>
+                 <button style={{border:'none', width:'40px', borderRadius:'50%', fontSize:'30px', backgroundColor:'goldenrod'}} onClick={(e)=>{addFile()}}> + </button>
+                 <input type="file"  style={{position:"absolute",right:"17rem", width:"10%", opacity:0}} onChange={(e)=>{getFile(e)}}/>
                 </div>
 
-                 <div style={{height:"80%"}}>
-                 <div>
-                 <div style={{display:'flex', flexDirection:'row' , alignItems:'center', alignText:'center', height:'3rem' }}> 
-              <InsertDriveFileIcon style={{color:"goldenrod", height:"100%", width:'20%'}}/>
-               <p style={{height:'50%' , margin:0}}>Main File   6 </p>
-              </div>
-              <div style={{height:'.01rem', width:'80%', backgroundColor:'grey' }}></div>
+                {files.map((files,i)=>{
+               return (
+                <div style={{height:"80%"}}>
+                <div>
+                <div style={{display:'flex', flexDirection:'row' , alignItems:'center', alignText:'center', height:'3rem', }}> 
+             <a src={files} style={{color:"goldenrod", height:"100%", width:'20%'}}/>
+              <p style={{height:'50%' , margin:0}}>{files.name} </p>
+             </div>
+             <div style={{height:'.01rem', width:'80%', backgroundColor:'grey' }}></div>
 
-                 </div>
+                </div>
+                <br />
               
+           
 
-                 </div>
-                 
+                </div>
+                
+               )
+
+                })}
+                
                  
               </div>
                 
