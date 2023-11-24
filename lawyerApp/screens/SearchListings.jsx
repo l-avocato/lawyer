f9f7d2518ad42446e4ccf50225c6035e1f4e7d21
azizarf <aziz95arfaoui@gmail.com>
2023-11-24 19:20:57 +0100

    done

--- lawyerApp/screens/SearchListings.jsx
+++ lawyerApp/screens/SearchListings.jsx
@@ -12,79 +12,80 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import { FIREBASE_DB } from "../firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 import { useRoute } from "@react-navigation/native";
-
-const SearchListings = () => {
+import haversine from "haversine";
+const SearchListings = ({ navigation }) => {
   const route = useRoute();
-  const filteredLawyers = route.params.filteredLawyers || [];
-  console.log(filteredLawyers, "params");
 
-  const handleArrowClick = () => {
-    // Handle arrow icon click here
-  };
-  const handleButtonPress = () => {
-    // Handle button press here
-    console.log("Button pressed");
-    // You can add your custom logic here
-  };
+  const filteredLawyers = route.params.filteredLawyers || [];
+
   const [isHeartPressed, setIsHeartPressed] = useState(false);
   const lawyersCollectionRef = collection(FIREBASE_DB, "lawyers");
   const [lawyers, setLawyers] = useState([]);
-  // const getLawyers = async () => {
-  //   try {
-  //     const result = await getDocs(lawyersCollectionRef);
-  //     const lawyers = result.docs.map((doc) => ({
-  //       ...doc.data(),
-  //       id: doc.id,
-  //     }));
-  //     setLawyers(lawyers);
-  //     console.log("this is lawyers hhhhhhhh", lawyers);
-  //   } catch (error) {
-  //     console.error("Error fetching data:", error);
-  //   }
-  // };
-  // useEffect(() => {
-  //   console.log("filteredLawyers in useEffect:", filteredLawyers);
-  //   console.log("lawyers in useEffect:", lawyers);
-  //   getLawyers();
-  // }, [filteredLawyers]);
+
   const renderItem = () => {
-    filteredLawyers.map((item) => console.log("itemmmmmmmmmmmmmmmm", item));
-    return filteredLawyers.map((item) => (
-      <View style={styles.lawyerView1}>
-        <Image source={{ uri: item.imageUrl }} style={styles.lawyerImage} />
-        <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
-          <Text style={styles.buttonText}>{item.fullName}</Text>
-        </TouchableOpacity>
-        <Text style={styles.additionalText}>{item.category.name}</Text>
-        <View style={styles.starIcon}>
-          <Icon name="star" size={18} color="#D5B278" />
-          <Text style={styles.textt}>{4.5}</Text>
+    // filteredLawyers.map((item) => console.log("itemmmmmmmmmmmmmmmm", item));
+
+    return filteredLawyers.map((item) => {
+      let distance = 0;
+      const lengthRating = item.ratings.length || 1;
+      let rating = item.ratings.reduce((acc, rat) => {
+        return acc + rat.stars;
+      }, 0);
+      rating = (rating / lengthRating).toFixed(1);
+      distance = haversine(
+        {
+          latitude: item.latitude,
+          longitude: item.langitude,
+        },
+        {
+          latitude: 36.87185,
+          longitude: 10.133079,
+        },
+      ).toFixed(1);
+      console.log("distance", distance);
+      return (
+        <View style={styles.lawyerView1}>
+          <TouchableOpacity
+            onPress={navigation.navigate("ProfilDetails", { item })}
+
+            // onPress={navigation.navigate("ProfilDetails", { item })}
+          >
+            <Image source={{ uri: item.ImageUrl }} style={styles.lawyerImage} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>{item.fullName}</Text>
+          </TouchableOpacity>
+          <Text style={styles.additionalText}>{item.category.name}</Text>
+          <View style={styles.starIcon}>
+            <Icon name="star" size={18} color="#D5B278" />
+            <Text style={styles.textt}>{rating}</Text>
+          </View>
+          <View style={styles.locationIcon}>
+            <Icon name="map-marker" size={20} color="blue" />
+            <Text style={styles.textt}>{distance} Km</Text>
+          </View>
+          <View style={styles.priceIcon}>
+            <Icon name="dollar" size={18} color="green" />
+            <Text style={styles.textt}>{item.Price}</Text>
+          </View>
+          {/* <TouchableOpacity
+            onPress={() => setIsHeartPressed(!isHeartPressed)}
+            style={styles.heartIcon}>
+            {isHeartPressed ? (
+              <Icon name="heart" size={20} color="red" />
+            ) : (
+              <Icon name="heart-o" size={20} color="red" />
+            )}
+          </TouchableOpacity> */}
         </View>
-        <View style={styles.locationIcon}>
-          <Icon name="map-marker" size={20} color="blue" />
-          <Text style={styles.textt}>1.5km</Text>
-        </View>
-        <View style={styles.priceIcon}>
-          <Icon name="dollar" size={18} color="green" />
-          <Text style={styles.textt}>{item.Price}</Text>
-        </View>
-        <TouchableOpacity
-          onPress={() => setIsHeartPressed(!isHeartPressed)}
-          style={styles.heartIcon}>
-          {isHeartPressed ? (
-            <Icon name="heart" size={20} color="red" />
-          ) : (
-            <Icon name="heart-o" size={20} color="red" />
-          )}
-        </TouchableOpacity>
-      </View>
-    ));
+      );
+    });
   };
 
   return (
     <View>
       <View style={styles.header}>
-        <TouchableOpacity onPress={handleArrowClick} style={styles.arrowIcon}>
+        <TouchableOpacity style={styles.arrowIcon}>
           <Icon name="arrow-left" size={20} color="white" />
         </TouchableOpacity>
         <Text style={styles.headerText}>Search Listings</Text>
